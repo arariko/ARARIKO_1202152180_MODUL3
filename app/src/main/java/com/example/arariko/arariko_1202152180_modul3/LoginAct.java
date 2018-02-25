85fcaa26271cad11ca87ca45dbb4d7042a421b60
arariko <ararikopasa@gmail.com>
2018-02-25 22:29:54 +0700

    push

---
+++ app/src/main/java/com/example/arariko/arariko_1202152180_modul3/LoginAct.java
@@ -0,0 +1,35 @@
+package com.example.arariko.arariko_1202152180_modul3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginAct extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.txt_pwd);
+        btnLogin = findViewById(R.id.signin);
+    }
+
+    //    fungsi yang digunakan untuk login
+    public void showLogin(View view) {
+        if (username.getText().toString().equalsIgnoreCase("ead") && password.getText().toString().equalsIgnoreCase("mobile")){
+            Toast.makeText(this, "Welcome", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(LoginAct.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Username atau Password yang anda masukkan salah..... please try again", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/arariko/arariko_1202152180_modul3/SplashAct.java
@@ -0,0 +1,24 @@
+package com.example.arariko.arariko_1202152180_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginAct.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
